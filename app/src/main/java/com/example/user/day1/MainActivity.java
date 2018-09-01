56976d7ae7c2d35b247fe546fd99e22567fc04f5
HANMUK <gksanr4874@gmail.com>
2018-09-01 16:12:07 +0900

    Commit2

--- app/src/main/java/com/example/user/day1/MainActivity.java
+++ app/src/main/java/com/example/user/day1/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,19 +24,21 @@ public class MainActivity extends AppCompatActivity implements Gota.OnRequestPer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Gota.Builder(this)
-                .withPermissions(Manifest.permission.CALL_PHONE)
+                .withPermissions(Manifest.permission.SEND_SMS)
                 .requestId(1)
                 .setListener(this)
                 .check();
         final EditText phone_number = (EditText)findViewById(R.id.editText);
+        final EditText text = (EditText)findViewById(R.id.editText2);
         Button button = (Button)findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone_number.getText().toString()));
-                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PERMISSION_GRANTED) {
-                    startActivity(intent);
+               // Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts(text.getText().toString(), phone_number.getText().toString(),null));
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) == PERMISSION_GRANTED) {
+                    //startActivity(intent);
+                    SmsManager.getDefault().sendTextMessage(phone_number.getText().toString(), null, text.getText().toString(), null, null);
                 }
             }
         });
@@ -44,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements Gota.OnRequestPer
 
     @Override
     public void onRequestBack(int requestId, @NonNull GotaResponse gotaResponse) {
-        if (gotaResponse.isGranted(Manifest.permission.CALL_PHONE)) {
+        if (gotaResponse.isGranted(Manifest.permission.SEND_SMS)) {
             // Your Code
         }
     }
